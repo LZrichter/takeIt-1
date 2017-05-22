605c356ab87e6b074d97b42dcdb3df348c977601
anthonytailer <aalmeida@inf.ufsm.br>
2017-05-22 10:17:09 -0300

    chat view modifications

--- takeit/views/chat.php
+++ takeit/views/chat.php
@@ -1,3 +1,40 @@
+<div class="container-fluid">
+	<h3><span class="fa fa-product-hunt"></span> Requisições do Item {Nome}</h3>
+	<hr>
+	<div class="panel panel-default">
+		<div class="panel-heading">
 
+			<div class="row">
+				<div id="left-head-chat" class="col-md-4">
+					<span class="fa fa-user-circle"> João da Silva</span>
+				</div>
+				<div id="right-head-chat" class="col-md-8">
+					<div class="align-item-right">
+						<input class="btn btn-success" type="button" name="chat-doar" value="Doar Item">
+						<div class="dropdown">
+							<button class="btn btn-default dropdown-toggle" data-toggle="dropdown">
+								<span class="fa fa-cog"><span class="caret"></span></span>
+							</button>
+						  <ul class="dropdown-menu">
+						    <li><a href="#">Enviar meus Contatos</a></li>
+						    <li><a href="#">Encerrar bate papo</a></li>
+						    <li><a href="#">Reportar bate papo</a></li>
+						  </ul>
+						</div>
+						<input id="qtd-intens-input" class="form-control input-sm" type="number" name="qtd-itens" value="1">
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="panel-body">
+			<div class="alert alert-danger">
+				Teste
+			</div>
+			<div class="alert alert-success">
+				Teste
+			</div>
+		</div>
+	</div>
+</div>
 </main><!-- #page-content-wrapper  -->
 </div> <!-- #wrapper -->

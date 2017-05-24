0c504cf36437c9627fded1436da1f560151b9608
anthonytailer <aalmeida@inf.ufsm.br>
2017-05-24 03:03:21 -0300

    menu bugado troquei tudo porra fiz do zero sa merda

--- takeit/views/templates/menuDoacoes.php
+++ takeit/views/templates/menuDoacoes.php
@@ -1,6 +1,6 @@
-<div id="wrapper" class="footer-align">
-        <div id="sidebar-wrapper">
-            <aside id="sidebar">
+<!-- <div id="wrapper" class="footer-align">
+        <div id="sidebar-wrapper"> -->
+            <!-- <aside id="sidebar">
                 <ul id="sidemenu" class="sidebar-nav">
                     <li>
                         <a id="location" class="accordion-toggle collapsed toggle-switch" data-toggle="collapse" href="#submenu-1">
@@ -52,7 +52,64 @@
                         </ul>
                     </li>
                 </ul>
-            </aside>            
-        </div>
-        <main id="page-content-wrapper" role="main">
+            </aside> -->            
+<nav class="navbar navbar-default sidebar" role="navigation">
+    <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-sidebar-navbar-collapse-1">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>  
+      <div class="navbar-brand">Filtros</div>    
+    </div>
+    <div class="collapse navbar-collapse" id="bs-sidebar-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown">Estado <span class="caret"></span><span style="font-size:20px;" class="pull-right  showopacity fa fa-map-marker"></span></a>
+          <ul class="dropdown-menu forAnimate showopacity" role="menu">
+            <li>
+                <label for="sel1">Estado: </label>
+                <select class="" id="sel1">
+                    <option>test</option>
+                    <option>test</option>
+                    <option>test</option>
+                    <option>test</option>
+                </select>
+            </li>
+            <li>
+                <label for="sel2">Município: </label>
+                <select class="" id="sel2">
+                    <option>test</option>
+                    <option>test</option>
+                    <option>test</option>
+                    <option>test</option>
+                </select>
+            </li>
+          </ul>
+        </li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown">Categorias <span class="caret"></span><span style="font-size:20px;" class="pull-right  showopacity fa fa-list"></span></a>          
+            <ul id="submenu-2" class="dropdown-menu forAnimate showopacity" role="menu">
+                <li><a href="#"> Roupas</a></li>
+                <li><a href="#"> Sapatos</a></li>
+                <li><a href="#"> Móveis</a></li>
+                <li><a href="#"> Eletrodomésticos</a></li>
+                <li><a href="#"> ..</a></li>
+                <li><a href="#"> ..</a></li>
+                <li><a href="#"> ..</a></li>
+                <li><a href="#"> ..</a></li>
+                <li><a href="#"> ..</a></li>
+                <li><a href="#"> ..</a></li>
+                <li><a href="#"> ..</a></li>
+                <li><a href="#"> ..</a></li>
+                <li><a href="#"> ..</a></li>
+            </ul>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+<div class="main">
         

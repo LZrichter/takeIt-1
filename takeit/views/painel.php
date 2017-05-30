39976317c331e579f07444828b5c6c71f322a9bf
lucaslioli <lucaslima.o@outlook.com>
2017-05-29 23:21:33 -0300

    Criado front-end painel

--- takeit/views/painel.php
+++ takeit/views/painel.php
@@ -3,6 +3,51 @@
 </ul>
 
 <div class="container footer-align" id="main">
-	<p>ESSE PAINEL ESTA MUITO BONITO</p>
-	<a href="painel/perfil">Pefil do Usuário</a>
+	<div class="panel-heading text-center">
+		<h1><span class="fa fa-dashboard"></span> Painel Administrativo</h1>
+	</div>
+	<ul class="flex-container">
+		<!-- BOTÃO 1 -->
+	  	<li class="flex-item">
+		  	<a href="/painel/notificacoes">
+		  		<img src="<?=base_url()?>/assets/img/painel_notificacao.png">
+		  		<h2>Notificações</h2>
+		  	</a>
+	  	</li>
+	  	<!-- BOTÃO 2 -->
+	  	<li class="flex-item">
+		  	<a href="/painel/notificacoes">
+		  		<img src="<?=base_url()?>/assets/img/painel_doar.png">
+		  		<h2>Chat Doações</h2>
+		  	</a>
+	  	</li>
+	  	<!-- BOTÃO 3 -->
+	  	<li class="flex-item">
+		  	<a href="/painel/itens_doados">
+		  		<img src="<?=base_url()?>/assets/img/painel_doados.png">
+		  		<h2>Itens Doados</h2>
+		  	</a>
+	  	</li>
+	  	<!-- BOTÃO 4 -->
+	  	<li class="flex-item">
+		  	<a href="/painel/perfil">
+		  		<img src="<?=base_url()?>/assets/img/painel_perfil.png">
+		  		<h2>Perfil do Usuário</h2>
+		  	</a>
+	  	</li>
+	  	<!-- BOTÃO 5 -->
+	  	<li class="flex-item">
+		  	<a href="/painel/interesses">
+		  		<img src="<?=base_url()?>/assets/img/painel_receber.png">
+		  		<h2>Chat Interesses</h2>
+		  	</a>
+	  	</li>
+	  	<!-- BOTÃO 6 -->
+	  	<li class="flex-item">
+		  	<a href="/painel/itens_recebidos">
+		  		<img src="<?=base_url()?>/assets/img/painel_recebidos.png">
+		  		<h2>Itens Recebidos</h2>
+		  	</a>
+	  	</li>
+	</ul>
 </div>

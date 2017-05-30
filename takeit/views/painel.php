1578da43f6a8610f019e6e97f90f0e87ba916053
lucaslioli <lucaslima.o@outlook.com>
2017-05-30 00:56:56 -0300

    Criado front-end perfil do usuário

--- takeit/views/painel.php
+++ takeit/views/painel.php
@@ -9,44 +9,56 @@
 	<ul class="flex-container">
 		<!-- BOTÃO 1 -->
 	  	<li class="flex-item">
-		  	<a href="/painel/notificacoes">
-		  		<img src="<?=base_url()?>/assets/img/painel_doar.png">
-		  		<h2>Itens para Doar</h2>
+		  	<a href="/painel/ofertas">
+		  		<div>
+		  			<img src="<?=base_url()?>/assets/img/painel_doar.png">
+		  			<h2>Itens para Doar</h2>
+		  		</div>
 		  	</a>
 	  	</li>
 	  	<!-- BOTÃO 2 -->
 	  	<li class="flex-item">
 		  	<a href="/painel/itens_doados">
-		  		<img src="<?=base_url()?>/assets/img/painel_doados.png">
-		  		<h2>Itens Doados</h2>
+		  		<div>
+		  			<img src="<?=base_url()?>/assets/img/painel_doados.png">
+		  			<h2>Itens Doados</h2>
+		  		</div>
 		  	</a>
 	  	</li>
 	  	<!-- BOTÃO 3 -->
 	  	<li class="flex-item">
 		  	<a href="/doacoes">
-		  		<img src="<?=base_url()?>/assets/img/painel_doacoes.png">
-		  		<h2>Todas Doações</h2>
+		  		<div>
+		  			<img src="<?=base_url()?>/assets/img/painel_doacoes.png">
+		  			<h2>Todas Doações</h2>
+		  		</div>
 		  	</a>
 	  	</li>
 	  	<!-- BOTÃO 4 -->
 	  	<li class="flex-item">
 		  	<a href="/painel/interesses">
-		  		<img src="<?=base_url()?>/assets/img/painel_receber.png">
-		  		<h2>Itens para Receber</h2>
+		  		<div>
+		  			<img src="<?=base_url()?>/assets/img/painel_receber.png">
+		  			<h2>Itens para Receber</h2>
+		  		</div>
 		  	</a>
 	  	</li>
 	  	<!-- BOTÃO 5 -->
 	  	<li class="flex-item">
 		  	<a href="/painel/itens_recebidos">
-		  		<img src="<?=base_url()?>/assets/img/painel_recebidos.png">
-		  		<h2>Itens Recebidos</h2>
+		  		<div>
+		  			<img src="<?=base_url()?>/assets/img/painel_recebidos.png">
+		  			<h2>Itens Recebidos</h2>
+		  		</div>
 		  	</a>
 	  	</li>
 	  	<!-- BOTÃO 6 -->
 	  	<li class="flex-item">
 		  	<a href="/painel/perfil">
-		  		<img src="<?=base_url()?>/assets/img/painel_perfil.png">
-		  		<h2>Perfil do Usuário</h2>
+		  		<div>
+		  			<img src="<?=base_url()?>/assets/img/painel_perfil.png">
+		  			<h2>Perfil do Usuário</h2>
+		  		</div>
 		  	</a>
 	  	</li>
 	</ul>
